beb5015abe5c9a26cd03781f00cb913b232830ce
Agrim <masteragrim11@gmail.com>
2026-06-12 19:05:51 +0530

    feat: mount confirm provider and load base styles

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,10 +1,14 @@
 import { createRoot } from 'react-dom/client'
+import './tokens.css'
 import './global.css'
 import App from './App.jsx'
 import { BrowserRouter } from 'react-router-dom'
+import { ConfirmProvider } from './components/ConfirmProvider.jsx'
 
 createRoot(document.getElementById('root')).render(
   <BrowserRouter>
-    <App/>
+    <ConfirmProvider>
+      <App/>
+    </ConfirmProvider>
   </BrowserRouter>
 )
